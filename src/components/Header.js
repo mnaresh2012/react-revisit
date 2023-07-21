ab565d2a632ac6c2ae860a0da590445179151c3f
Naresh_Mahanta <naresh.mahanta@valtech.com>
2023-07-21 07:24:00 +0530

    added bootstrap and header, footer component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 export const Header = () => {
     return (
         <>
-            <h2>Header Component</h2>
+            <div className="container">
+                <header className="d-flex justify-content-center py-3">
+                <ul className="nav nav-pills">
+                    <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link">About</a></li>
+                </ul>
+                </header>
+            </div>
         </>
     );
 }
