df841a83a8f5119d3fa37e86188da1aa62285473
Naresh_Mahanta <naresh.mahanta@valtech.com>
2023-07-21 07:39:35 +0530

    added jumbotron

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,11 @@
+export const Jumbotron = () => {
+    return (
+        <div className="p-5 mb-4 bg-body-tertiary rounded-3">
+            <div className="container-fluid py-5">
+                <h1 className="display-5 fw-bold">Custom jumbotron</h1>
+                <p className="col-md-12 fs-4 text-center">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+                <button className="btn btn-primary btn-lg" type="button">Example button</button>
+            </div>
+        </div>
+    );
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 
 import {Header} from './Header';
 import Footer from './Footer';
+import { Jumbotron } from "./Jumbotron";
 
 class Main extends Component {
     constructor() {
@@ -11,7 +12,25 @@ class Main extends Component {
         return (
             <div>
                 <Header />
-                    Main Component.
+                <div className="container">
+                    <Jumbotron />
+                    <div class="row align-items-md-stretch">
+                        <div class="col-md-6">
+                            <div class="h-100 p-5 text-bg-dark rounded-3">
+                            <h2>Change the background</h2>
+                            <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+                            <button class="btn btn-outline-light" type="button">Example button</button>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="h-100 p-5 bg-body-tertiary border rounded-3">
+                            <h2>Add borders</h2>
+                            <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
+                            <button class="btn btn-outline-secondary" type="button">Example button</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <Footer />
             </div>
         );
